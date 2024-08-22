1d7e59b4ffa423138aabdbddd8a15dbda02a049b
dsarika2422 <156677062+dsarika2422@users.noreply.github.com>
2024-08-22 23:50:17 +0530

    Add files via upload

---
+++ 1A/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	 public static void main(String[] args) {
+	        Logger logger = Logger.getInstance();
+	        logger.log("Application started.");
+	        logger.log("An event occurred.");
+	        logger.log("Application finished.");
+	    }
+	}
+
